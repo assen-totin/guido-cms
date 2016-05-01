40cf97658a0e52cbf810bc7a13288010797bb199
Assen Totin <assen.totin@gmail.com>
2016-05-02 00:59:38 +0300

    Add default upload hierarchy.

--- api/admin_upload.js
+++ api/admin_upload.js
@@ -1,5 +1,18 @@
 // Upload a file
 
+var sanitise = function(path, type) {
+	var ret = path;
+
+	switch(type) {
+		case 'name':
+			ret = ret.replace('/', '_');
+		case 'dir':
+			ret = ret.replace('..', '__');
+	}
+
+	return ret;
+};
+
 var init = function(params, callback) {
 	var sqlClient = params.clients.sqlClient;
 	var logger = params.clients.logger;
@@ -19,30 +32,46 @@ var init = function(params, callback) {
 		return;
 	}
 
-	// Move the file to the selected path (which must be a writable directory)
-	var pathDest = params.documentRoot + params.query.path;
+	// See if we were given a path; else, set a default hierarchy
+	var pathDest;
 	var stat;
-	try {
-		stat = fs.lstatSync(pathDest);
-		logger.debug('Got stat data for file ' + pathDest);
-	}
-	catch (e) {
-		callback({code:404, msg: "Upload path not found: " + pathDest});
-		return;
-	}
+	if (params.query.path) {
+		pathDest = params.documentRoot + '/upload' + sanitise(params.query.path, 'dir');
+		
+		try {
+			stat = fs.lstatSync(pathDest);
+			logger.debug('Got stat data for file ' + pathDest);
+		}
+		catch (e) {
+			callback({code:404, msg: "Upload path not found: " + pathDest});
+			return;
+		}
 
-	if (! stat.isDirectory()) {
-		callback({code:400, msg: "Upload path is not a directory: " + pathDest});
-		return;
+		// Path must be a dir
+		if (! stat.isDirectory()) {
+			callback({code:400, msg: "Upload path is not a directory: " + pathDest});
+			return;
+		}
 	}
+	else {
+		var d = new Date();
+		pathDest = params.documentRoot + '/upload' + d.getFullYear() + '/' + d.getMonth();
 
-	// Sanitise file name
-	var fileName = params.upload.file.name.replace('/', '_');
+		try {
+			stat = fs.lstatSync(pathDest);
+			logger.debug('Got stat data for file ' + pathDest);
+		}
+		catch (e) {
+			// Create the directory
+			fs.mkdirSync(params.documentRoot + '/upload' + d.getFullYear(), 755);
+			fs.mkdirSync(pathDest, 755);
+		}		
+	}
 
 	// Append slash if missing and move the file
 	if (pathDest.slice(-1) != '/')
 		pathDest += '/';
-	pathDest += fileName;
+	pathDest += sanitise(params.upload.file.name, 'name');
 
 	fs.renameSync(pathSource, pathDest);
 
